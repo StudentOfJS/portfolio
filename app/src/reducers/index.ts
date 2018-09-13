ea9b28155c6cc1d83aaec25ad02b93d1ba8a5d56
StudentOfJS <rodlewis45@gmail.com>
2018-09-13 22:39:46 +0200

    create RootState

--- app/src/reducers/index.ts
+++ app/src/reducers/index.ts
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
 
-import cvReducer from './cv_reducer';
-import projectsReducer from './projects_reducer';
-import { RootState } from '../store';
+import cvReducer, { CVState } from './cv_reducer';
+import projectsReducer, { ProjectState } from './projects_reducer';
+
+interface StoreEnhancerState {
+}
+
+export interface RootState extends StoreEnhancerState {
+  cv: CVState;
+  projects: ProjectState;
+}
 
 export default combineReducers<RootState>({
   cv: cvReducer,
