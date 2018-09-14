f95de92500c31fad231e272b3a8f3dc13a3b80d7
StudentOfJS <rodlewis45@gmail.com>
2018-09-14 13:29:53 +0200

    add educationList

---
+++ app/src/components/CV/EducationList.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { RootState } from '../../reducers';
+import { Education } from '../../proto/portfolio_pb';
+import { Item } from 'semantic-ui-react';
+
+interface EducationProps {
+  courses: Education.AsObject;
+}
+
+class EducationList extends React.Component<EducationProps, {}> {
+  public render() {
+    const { courses } = this.props;
+    if (courses) {
+      return (
+        <Item.Group>
+          {
+            courses.coursesList.map(course => (
+              <Item>
+                <Item.Image size="tiny" src={course.institution} />
+
+                <Item.Content>
+                  <Item.Header>{course.name} - {course.institution}</Item.Header>
+                  <Item.Meta>
+                    <span className="stay">{course.dates}</span>
+                  </Item.Meta>
+                  <Item.Description>{course.description}</Item.Description>
+                </Item.Content>
+              </Item>
+            ))
+          }
+        </Item.Group>
+      );
+    }
+    return;
+  }
+}
+
+function mapStateToProps(state: RootState) {
+  return {
+    courses: state.cv.education,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+)(EducationList);
