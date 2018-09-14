94e6e334f46b91cd736accc595eb414db9280abe
StudentOfJS <rodlewis45@gmail.com>
2018-09-14 14:14:49 +0200

    create and add ratings component to skills

---
+++ app/src/components/CV/Rating.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Icon } from 'semantic-ui-react';
+interface RatingProps {
+  rating: number;
+}
+
+const Rating: React.SFC<RatingProps> = ({ rating }) => {
+  const good = Array(rating).fill(<Icon name="star" size="small" color="orange" />);
+  const bad = Array(10 - rating).fill(<Icon name="star outline" size="small" />);
+  return (
+    <div>
+      {good}{bad}
+    </div>
+  );
+};
+
+export default Rating;
--- app/src/components/CV/SkillsList.tsx
+++ app/src/components/CV/SkillsList.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { RootState } from '../../reducers';
 import { Skills } from '../../proto/portfolio_pb';
+import { List, Icon } from 'semantic-ui-react';
+import Rating from './Rating';
 
 interface SkillProps {
   skills: Skills.AsObject;
@@ -9,9 +11,15 @@ interface SkillProps {
 
 class SkillsList extends React.Component<SkillProps, {}> {
   public render() {
-
+    const { skills } = this.props;
     return (
-
+      <List>
+        {
+          skills.skillsList.map(skill => (
+            <List.Item>{skill.name} - {skill.description} - <Rating rating={skill.rating} /></List.Item>
+          ))
+        }
+      </List>
     );
   }
 }
