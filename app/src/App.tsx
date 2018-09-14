86384daf16af5df5a553a2e97d325964ce304e34
StudentOfJS <rodlewis45@gmail.com>
2018-09-14 15:38:56 +0200

    tabular routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,74 +1,32 @@
 import * as React from 'react';
-import { connect, Dispatch } from 'react-redux';
-import { portfolio, RootAction } from './actions';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, RouteComponentProps } from 'react-router-dom';
 import { Input, Menu, Segment } from 'semantic-ui-react';
-import { RootState } from './reducers';
-import { CVState } from './reducers/cv_reducer';
-import { ProjectState } from './reducers/projects_reducer';
 import ProjectsView from './components/Projects/ProjectsView';
+import CvView from './components/CV/CvView';
 
-type IAppProps = {
-  cv: CVState,
-  projects: ProjectState,
-  fetchProjects: () => Promise<void>;
-  fetchCV: () => Promise<void>;
-};
+const App: React.SFC<RouteComponentProps> = ({ match }) => (
+  <Router>
+    <Menu attached="top" tabular={true}>
+      <NavLink to="/"><Menu.Item name="home" active={match.path === '/'} /></NavLink>
+      <NavLink to="/cv"><Menu.Item name="cv" active={match.path === '/cv'} /></NavLink>
+      <NavLink to="/projects"><Menu.Item name="projects" active={match.path === '/projects'} /></NavLink>
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Input
+            transparent={true}
+            icon={{ name: 'search', link: true }}
+            placeholder="Search CV..."
+          />
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
 
-class App extends React.Component<IAppProps, {}> {
-  public componentDidMount = async () => {
-    await this.props.fetchProjects();
-    await this.props.fetchCV();
-  }
-  public render() {
+    <Segment attached="bottom">
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/cv" component={CvView} />
+      <Route exact={true} path="/projects" component={ProjectsView} />
+    </Segment>
+  </Router>
+);
 
-    return (
-      <Router>
-        <Menu attached="top" tabular={true}>
-          <NavLink to="/"><Menu.Item name="home" active={activeItem === 'home'} /></NavLink>
-          <NavLink to="/cv"><Menu.Item name="cv" active={activeItem === 'cv'} /></NavLink>
-          <NavLink to="/projects"><Menu.Item name="projects" active={activeItem === 'projects'} /></NavLink>
-          <Menu.Menu position="right">
-            <Menu.Item>
-              <Input
-                transparent={true}
-                icon={{ name: 'search', link: true }}
-                placeholder="Search CV..."
-              />
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
-
-        <Segment attached="bottom">
-          <Route exact={true} path="/" component={Home} />
-          <Route path="/cv" component={CvView} />
-          <Route exact={true} path="/projects" component={ProjectsView} />
-        </Segment>
-      </Router>
-    );
-  }
-}
-
-function mapStateToProps(state: RootState) {
-  return {
-    cv: state.cv,
-    projects: state.projects
-  };
-}
-
-function mapDispatchToProps(dispatch: Dispatch<RootAction>) {
-  return {
-    fetchProjects: async () => {
-      await dispatch(portfolio.initializeProjects());
-    },
-    fetchCV: async () => {
-      await dispatch(portfolio.initializeBio());
-      await dispatch(portfolio.initializeCV());
-    }
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
--- app/src/components/CV/CvView.tsx
+++ app/src/components/CV/CvView.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { Route, NavLink, RouteComponentProps } from 'react-router-dom';
+
 import { connect, Dispatch } from 'react-redux';
 import { portfolio, RootAction } from '../../actions';
-import { Grid, Segment } from 'semantic-ui-react';
+import { Input, Menu, Segment } from 'semantic-ui-react';
 import SkillsList from './SkillsList';
 import ExperienceList from './ExperienceList';
 import Bio from './Bio';
@@ -11,37 +13,37 @@ type ICvViewProps = {
   fetchCV: () => Promise<void>;
 };
 
-class CvView extends React.Component<ICvViewProps, {}> {
+class CvView extends React.Component<RouteComponentProps & ICvViewProps> {
   public componentDidMount = async () => {
     await this.props.fetchCV();
   }
   public render() {
-
+    const { path } = this.props.match;
     return (
-      <Grid stackable={true} columns="equal">
-        <Grid.Row>
-          <Grid.Column>
-            <Bio />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <ExperienceList />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <Segment>
-              <SkillsList />
-            </Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>
-              <EducationList />
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <React.Fragment>
+        <Menu attached="top" tabular={true}>
+          <NavLink to="/cv/bio"><Menu.Item name="bio" active={path === '/cv/bio'} /></NavLink>
+          <NavLink to="/cv/education"><Menu.Item name="education" active={path === '/cv/education'} /></NavLink>
+          <NavLink to="/cv/experience"><Menu.Item name="experience" active={path === '/cv/experience'} /></NavLink>
+          <NavLink to="/cv/skills"><Menu.Item name="skills" active={path === '/cv/skills'} /></NavLink>
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Input
+                transparent={true}
+                icon={{ name: 'search', link: true }}
+                placeholder="Search CV..."
+              />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+
+        <Segment attached="bottom">
+          <Route path="/cv/bio" component={Bio} />
+          <Route path="/cv/education" component={EducationList} />
+          <Route path="/cv/experience" component={ExperienceList} />
+          <Route path="/cv/skills" component={SkillsList} />
+        </Segment>
+      </React.Fragment>
     );
   }
 }
