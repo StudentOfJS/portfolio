8877d1d05e14d24d26f0c6b8b49f10076b490088
StudentOfJS <rodlewis45@gmail.com>
2018-09-15 11:00:03 +0200

    server setup

--- server/main.go
+++ server/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi"
 	chiMiddleware "github.com/go-chi/chi/middleware"
-	"google.golang.org/grpc"
-	"net/http"
-	"github.com/easyCZ/grpc-web-hacker-news/server/hackernews"
-	hackernews_pb "github.com/easyCZ/grpc-web-hacker-news/server/proto"
-	"google.golang.org/grpc/grpclog"
-	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/go-chi/cors"
-	"github.com/easyCZ/grpc-web-hacker-news/server/proxy"
-	"github.com/easyCZ/grpc-web-hacker-news/server/middleware"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"github.com/studentofjs/portfolio/server/middleware"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
 )
 
 func main() {
@@ -25,18 +23,16 @@ func main() {
 	router.Use(
 		chiMiddleware.Logger,
 		chiMiddleware.Recoverer,
-		middleware.NewGrpcWebMiddleware(wrappedGrpc).Handler,	// Must come before general CORS handling
+		middleware.NewGrpcWebMiddleware(wrappedGrpc).Handler, // Must come before general CORS handling
 		cors.New(cors.Options{
-			AllowedOrigins: []string{"*"},
+			AllowedOrigins:   []string{"*"},
 			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", },
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 			ExposedHeaders:   []string{"Link"},
 			AllowCredentials: true,
 			MaxAge:           300, // Maximum value not ignored by any of major browsers
 		}).Handler,
 	)
-
-	router.Get("/article-proxy", proxy.Article)
 
 	if err := http.ListenAndServe(":8900", router); err != nil {
 		grpclog.Fatalf("failed starting http2 server: %v", err)
