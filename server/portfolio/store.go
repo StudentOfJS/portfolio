754399152955b9c6cb081f34aa2bd94a6993d0b5
StudentOfJS <rodlewis45@gmail.com>
2018-09-16 13:49:42 +0200

    stop exportting funcs and add grpc.Error

--- server/portfolio/store.go
+++ server/portfolio/store.go
@@ -6,9 +6,13 @@ import (
 	"github.com/google/uuid"
 	"github.com/asdine/storm"
 	"github.com/studentofjs/portfolio/server/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/metadata"
 )
-// AddBio adds the bio to the database
-func AddBio(title string, description string) error {
+
+func addBio(title string, description string) error {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	bio := proto.Bio{
@@ -16,12 +20,12 @@ func AddBio(title string, description string) error {
 		Description: description,
 	}
 	if err := db.Save(&bio); err != storm.ErrAlreadyExists {
-		return log.Errorf("bio already exists: %v", err)
+		return grpc.Errorf("bio already exists: %v", err)
 	}
 	return nil
 }
-// AddCourse adds a new course to the database
-func AddCourse(institution string, description string, dates string, name string) error {
+
+func addCourse(institution string, description string, dates string, name string) error {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	const id = uuid.New()
@@ -33,13 +37,13 @@ func AddCourse(institution string, description string, dates string, name string
 		Name: name
 	}
 	if err := db.Save(&course); err != storm.ErrAlreadyExists {
-		return log.Errorf("course already exists: %v", err)
+		return grpc.Errorf("course already exists: %v", err)
 	}
 	return nil
 }
 
-// AddJob adds a new skill to the database
-func AddJob(company, dates, description, jobTitle, location, logoUrl string) error {
+
+func addJob(company, dates, description, jobTitle, location, logoUrl string) error {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	const id = uuid.New()
@@ -54,13 +58,13 @@ func AddJob(company, dates, description, jobTitle, location, logoUrl string) err
 		Rating: rating,
 	}
 	if err := db.Save(&job); err != storm.ErrAlreadyExists {
-		return log.Errorf("job already exists: %v", err)
+		return grpc.Errorf("job already exists: %v", err)
 	}
 	return nil
 }
 
-// AddProject adds a new skill to the database
-func AddProject(description string, meta int32, title string) error {
+
+func addProject(description string, meta int32, title string) error {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	const id = uuid.New()
@@ -71,13 +75,13 @@ func AddProject(description string, meta int32, title string) error {
 		Description: description,
 	}
 	if err := db.Save(&project); err != storm.ErrAlreadyExists {
-		return log.Errorf("project already exists: %v", err)
+		return grpc.Errorf("project already exists: %v", err)
 	}
 	return nil
 }
 
-// AddSkill adds a new skill to the database
-func AddSkill(institution string, description string, rating int32, name string) error {
+
+func addSkill(institution string, description string, rating int32, name string) error {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	const id = uuid.New()
@@ -89,73 +93,73 @@ func AddSkill(institution string, description string, rating int32, name string)
 		Name: name,
 	}
 	if err := db.Save(&skill); err != storm.ErrAlreadyExists {
-		return log.Errorf("skill already exists: %v", err)
+		return grpc.Errorf("skill already exists: %v", err)
 	}
 	return nil
 }
 
-// GetBio gets the bio from the database
-func GetBio(title string) (proto.Bio, error) {
+
+func getBio(title string) (proto.Bio, error) {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	var bio proto.Bio
 	
 	if err := db.One("Title", title, &bio); err != nil {
-		return nil, log.Errorf("bio not found: %v", err)
+		return nil, grpc.Errorf("bio not found: %v", err)
 	}
 	return bio, nil
 }
 
-// GetEducation gets the courses from the database
-func GetEducation() (proto.Education, error) {
+
+func getEducation() (proto.Education, error) {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	var courses []proto.Course
 	
 	if err := db.All(&courses); err != nil {
-		return nil, log.Errorf("no course found: %v", err)
+		return nil, grpc.Errorf("no course found: %v", err)
 	}
 	var edu proto.Education
 	edu.Courses = courses
 	return edu, nil
 }
 
-// GetExperience gets the courses from the database
-func GetExperience() (proto.Experience, error) {
+
+func getExperience() (proto.Experience, error) {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	var jobs []proto.Job
 	
 	if err := db.All(&jobs); err != nil {
-		return nil, log.Errorf("no job found: %v", err)
+		return nil, grpc.Errorf("no job found: %v", err)
 	}
 	var experience proto.Experience
 	experience.Jobs = jobs
 	return experience, nil
 }
 
-// GetProjects gets the courses from the database
-func GetProjects() (proto.Projects, error) {
+
+func getProjects() (proto.Projects, error) {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	var p []proto.Project
 	
 	if err := db.All(&p); err != nil {
-		return nil, log.Errorf("no project found: %v", err)
+		return nil, grpc.Errorf("no project found: %v", err)
 	}
 	var projects proto.Projects
 	projects.Projects = p
 	return projects, nil
 }
 
-// GetSkills gets the courses from the database
-func GetSkills() (proto.Skills, error) {
+
+func getSkills() (proto.Skills, error) {
 	db, err := storm.Open("my.db")
 	defer db.Close()
 	var s []proto.Skill
 	
 	if err := db.All(&s); err != nil {
-		return nil, log.Errorf("no skill found: %v", err)
+		return nil, grpc.Errorf("no skill found: %v", err)
 	}
 	var skills proto.Skills
 	skills.Skills = s
