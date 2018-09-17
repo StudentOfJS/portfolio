3009da6352aaa0582c87005154af24942fb73920
StudentOfJS <rodlewis45@gmail.com>
2018-09-17 14:38:23 +0200

    add go client server and proto

--- protoc.sh
+++ protoc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 protoc \
     --go_out=plugins=grpc:./server \
+    --go_out=plugins=grpc:./client \
     --plugin=protoc-gen-ts=./app/node_modules/.bin/protoc-gen-ts \
     --ts_out=service=true:./app/src \
     --js_out=import_style=commonjs,binary:./app/src \
