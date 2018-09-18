775a21b0732277509a40d8ab4636b6b3baeb5b83
StudentOfJS <rodlewis45@gmail.com>
2018-09-18 19:11:30 +0200

    add go build to docker container

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@ echo "Installing NPM dependencies with yarn"
 (cd app && yarn install) &
 
 echo "Building go server"
-(cd server && go build -o server) &
+(cd server && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main && go build -o server) &
 
 echo "Building frontend application"
 (cd app && yarn build) &
+
+echo "Building empty docker container"
+docker build -t portfolio-container -f Dockerfile.scratch .
