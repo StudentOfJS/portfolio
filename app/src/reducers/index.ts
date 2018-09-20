14b087fee6c7d8d75fd31de1184c4cc42cc5072f
StudentOfJS <rodlewis45@gmail.com>
2018-09-20 14:42:30 +0200

    change reducer to reflect new proto file

--- app/src/reducers/index.ts
+++ app/src/reducers/index.ts
@@ -1,17 +1,14 @@
 import { combineReducers } from 'redux';
 
 import cvReducer, { CVState } from './cv_reducer';
-import projectsReducer, { ProjectState } from './projects_reducer';
 
 interface StoreEnhancerState {
 }
 
 export interface RootState extends StoreEnhancerState {
   cv: CVState;
-  projects: ProjectState;
 }
 
 export default combineReducers<RootState>({
   cv: cvReducer,
-  projects: projectsReducer
 });
