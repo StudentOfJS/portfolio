a4544746f6bec9230307495c622d9e98f5f88140
StudentOfJS <rodlewis45@gmail.com>
2018-09-22 12:00:31 +0200

    projects cards

--- app/src/components/Projects/ProjectsList.tsx
+++ app/src/components/Projects/ProjectsList.tsx
@@ -1,9 +1,47 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Card, Icon, List } from 'semantic-ui-react';
+import { Card, Icon } from 'semantic-ui-react';
 import { RootState } from '../../store';
-import * as images from './images';
+import images from './images';
 import { Projects } from '../../proto/portfolio_pb';
+import styled from '../../theme';
+
+const ProjectsContainer = styled.div`
+  align-items: center;
+  background-color: #666666;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-evenly;
+  width: 100vw;
+`;
+
+const ProjectsDisplay = styled.div`
+  align-items: space-evenly;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  height: 100%;
+  justify-content: space-evenly;
+  max-width: 1000px;
+  width: 100%;
+`;
+const CardContainer = styled.div`
+  background-color: white;
+  border-radius: 5px;
+  height: 380px;
+  margin: 10px;
+  padding: 1px;
+`;
+
+const ProjectsTitle = styled.h2`
+  color: white;
+  font-family: ${props => props.theme.fontFamily};
+  font-size: 50px;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+`;
 
 const extra = (keywords: string) => (
   <a>
@@ -22,21 +60,24 @@ class ProjectsList extends React.Component<ProjectsProps, {}> {
     const { p } = this.props;
 
     return (
-      <List animated={true} verticalAlign="middle">
-        {p && p.projectsList.map(project => {
-          return (
-            <List.Item key={project.id}>
-              <Card
-                image={images[project.id]}
-                header={project.title}
-                description={project.description}
-                extra={extra(project.meta)}
-              />
-            </List.Item>
-          );
-        })}
-      </List>
-
+      <ProjectsContainer>
+        <ProjectsTitle>Projects</ProjectsTitle>
+        <ProjectsDisplay>
+          {p && p.projectsList.map(project => {
+            return (
+              <CardContainer key={project.id}>
+                <Card
+                  style={{ height: '100%' }}
+                  image={images[project.id]}
+                  header={project.title}
+                  description={project.description}
+                  extra={extra(project.meta)}
+                />
+              </CardContainer>
+            );
+          })}
+        </ProjectsDisplay>
+      </ProjectsContainer>
     );
   }
 }
--- app/src/components/Projects/images/index.ts
+++ app/src/components/Projects/images/index.ts
@@ -1,8 +1,15 @@
+import * as p from './portfolio.png';
+import * as m from './moonrock.png';
+import * as ms from './moonrock-server.png';
+import * as w from './weathershiz.png';
+import * as s from './survey-management.png';
+import * as g from './gin-go.png';
+
 export default {
-  1: './portfolio.png',
-  2: './moonrock.png',
-  3: './moonrock-server.png',
-  4: './weathershiz.png',
-  5: './survey-management.png',
-  6: './gin-go.png'
+  1: p,
+  2: m,
+  3: ms,
+  4: w,
+  5: s,
+  6: g
 };
