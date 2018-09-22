69ff9fe40ebcb9093ac4bc09e4193d018a02a8eb
StudentOfJS <rodlewis45@gmail.com>
2018-09-22 10:10:48 +0200

    add key to rating icons

--- app/src/components/CV/Skills/Rating.tsx
+++ app/src/components/CV/Skills/Rating.tsx
@@ -5,8 +5,8 @@ interface RatingProps {
 }
 
 const Rating: React.SFC<RatingProps> = ({ rating }) => {
-  const good = Array(rating).fill(<Icon name="star" size="large" color="orange" />);
-  const bad = Array(10 - rating).fill(<Icon name="star outline" size="large" color="grey" />);
+  const good = Array(rating).fill(0).map((_, i) => <Icon key={i} name="star" size="large" color="orange" />);
+  const bad = Array(10 - rating).fill(0).map((_, i) => <Icon key={i} name="star outline" size="large" color="grey" />);
   return (
     <div>
       {good}{bad}
