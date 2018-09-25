bb60146a27e2a7a141406c7801f1b4f76a5e4784
StudentOfJS <rodlewis45@gmail.com>
2018-09-25 17:56:37 +0200

    update build process

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+echo "Removing old build from app server"
+(cd appServe && rm -rf /build) &
+
 echo "Installing NPM dependencies with yarn"
 (cd app && yarn install) &
 
-echo "Building go server"
-(cd server && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main && go build -o server) &
+echo "Building frontend application and copying to app server"
+(cd app && yarn build && cp /build ../appServe/build) &
 
-echo "Building frontend application"
-(cd app && yarn build) &
+echo "Building app server"
+(cd appServe && go build -o app-server) &
+
+echo "Building gRPC server"
+(cd server && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o gRPC && go build -o server) &
 
 echo "Building empty docker container"
 docker build -t portfolio-container -f Dockerfile.scratch .
