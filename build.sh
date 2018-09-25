d8092fc0969e3992cdd5b0b019d24015c470ecb9
StudentOfJS <rodlewis45@gmail.com>
2018-09-25 18:05:11 +0200

    update build process

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ echo "Building app server"
 (cd appServe && go build -o app-server) &
 
 echo "Building gRPC server"
-(cd server && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o gRPC && go build -o server) &
+(cd server && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o gRPC-server .) &
 
 echo "Building empty docker container"
 docker build -t portfolio-container -f Dockerfile.scratch .
